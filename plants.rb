afe9e3189acbe478a9dd7b409a882e35cab3a2e6
Joseph Lee <jhjlee702@gmail.com>
2023-08-02 13:27:55 -0600

    Change and to or

--- plants.rb
+++ plants.rb
@@ -1,7 +1,7 @@
-gets_sunshine = true
-ounces_of_water = 10
+gets_sunshine = false
+ounces_of_water = 8
 
-if gets_sunshine==true && ounces_of_water>=10
+if gets_sunshine==true || ounces_of_water>=10
     puts "The plant will thrive"
 else 
     puts "The plant will die"
